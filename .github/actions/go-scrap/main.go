fd1cc04bd5e1f5d03aa1652ec86a09a0849c07b1
nazeem <nazeemnob18@gmail.com>
2022-04-17 13:48:12 +0530

    go finished

---
+++ .github/actions/go-scrap/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func getInput(name string) string {
+	name = fmt.Sprintf("INPUT_%s", strings.ToUpper(name))
+	val := os.Getenv(name)
+	return strings.TrimSpace(val)
+}
+
+
+func main() {
+	fmt.Println(getInput("committer_username"))
+	fmt.Println(getInput("committer_email"))
+	fmt.Println(getInput("commit_message"))
+	fmt.Println(getInput("gh_token"))
+}
